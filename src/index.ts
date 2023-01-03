8fed4aa8ce72b4fab6f3a5635fb94e947cd9e4ec
yanyunchangfeng <yanyunchangfeng@gmail.com>
2023-01-03 15:45:54 +0800

    feat:add hash & browser history router

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,60 @@
-
 class App {
-    router:any;
-    linkList:any;
-    constructor(){
-        this.router = 'router-view';
-        this.initEvent();
+  router: any;
+  linkList: any;
+  constructor() {
+    this.router = "router-view";
+    this.initEvent();
+  }
+  initEvent = () => {
+    this.router = document.getElementById(this.router);
+    this.linkList = document.querySelectorAll('a[href^="/"]');
+    this.linkList.forEach((el: HTMLElement, index: number) =>
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+        history.pushState({ id: index }, "", el.getAttribute("href"));
+        this.popState();
+      })
+    );
+    window.addEventListener("DOMContentLoaded", this.hashChange);
+    window.addEventListener("DOMContentLoaded", this.popState);
+    window.addEventListener("hashchange", this.hashChange);
+    window.addEventListener("popstate", this.popState);
+  };
+  popState = () => {
+    switch (location.pathname) {
+      case "/blog":
+        this.router.innerHTML = "这是history bolg";
+        break;
+      case "/code":
+        this.router.innerHTML = "这是histoy code";
+        break;
+      case "/text":
+        this.router.innerHTML = "这是history text";
+        break;
+      case "/design":
+        this.router.innerHTML = "这是history design";
+        break;
+      default:
+        this.router.innerHTML = "这是history 404页面";
     }
-    initEvent = () => {
-       this.router = document.getElementById(this.router);
-       this.linkList = document.querySelectorAll('a[href^="/"]');
-       this.linkList.forEach((el:HTMLElement,index:number) => el.addEventListener('click', e => {
-           e.preventDefault();
-           history.pushState({id:index},'', el.getAttribute('href'));
-           this.popState();
-       }
-       ));
-       window.addEventListener('DOMContentLoaded',this.hashChange);
-       window.addEventListener('DOMContentLoaded',this.popState);
-       window.addEventListener('hashchange',this.hashChange);
-       window.addEventListener('popstate',this.popState);
+  };
+  hashChange = () => {
+    switch (location.hash) {
+      case "#/blog":
+        this.router.innerHTML = "这是 hash bolg页面";
+        break;
+      case "#/code":
+        this.router.innerHTML = "这是 hash code页面";
+        break;
+      case "#/text":
+        this.router.innerHTML = "这是 hash text页面";
+        break;
+      case "#/design":
+        this.router.innerHTML = "这是 hash design页面";
+        break;
+      default:
+        this.router.innerHTML = "这是 hash 404页面";
     }
-    popState = () => {
-       switch(location.pathname){
-           case '/blog':
-               this.router.innerHTML = "这是history bolg";
-               break;
-           case '/code':
-               this.router.innerHTML = "这是histoy code";
-               break;
-           case '/text':
-               this.router.innerHTML = "这是history text";
-               break;
-           case '/design':
-               this.router.innerHTML = "这是history design";
-               break;
-           default:
-               this.router.innerHTML = "这是history 404页面"    
-       }
-    }
-    hashChange = () => {
-        switch(location.hash){
-            case '#/blog':
-                this.router.innerHTML = "这是 hash bolg页面";
-                break;
-            case '#/code':
-                this.router.innerHTML = "这是 hash code页面";
-                break;
-            case '#/text':
-                this.router.innerHTML = "这是 hash text页面";
-                break;
-            case '#/design':
-                this.router.innerHTML = "这是 hash design页面";
-                break;
-                default:
-                this.router.innerHTML = "这是 hash 404页面"
-        }
-    }
+  };
 }
 let sys = new App();
-
-declare var module:any;
-
-if(module.hot){
-//    module.hot.accept();
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
-module.exports = (env) => {
-   return require(`./config/webpack.${env}.js`)
-}
+module.exports = (env, argv) => {
+  env = process.env.NODE_ENV === "development" ? "dev" : "prod";
+  return require(`./config/webpack.${env}`);
+};
